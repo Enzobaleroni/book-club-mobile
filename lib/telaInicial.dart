517e4b992265a5d923bc4383134180a3c0a29801
Enzobaleroni <167221537+Enzobaleroni@users.noreply.github.com>
2026-05-15 09:51:21 -0300

    Add files via upload

---
+++ lib/telaInicial.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Telainicial extends StatefulWidget {
+  const Telainicial({super.key});
+
+  @override
+  State<Telainicial> createState() => _TelainicialState();
+}
+
+class _TelainicialState extends State<Telainicial> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
